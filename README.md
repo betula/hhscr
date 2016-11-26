16f2d3f6724dd5dc52cb19b3567fc46c8e6eda00
Viacheslav Bereza <betula.icons8@gmail.com>
2016-11-26 05:25:33 +0300

    Add glob and re2 command line flags

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hhscr
+
+go install
--- main.go
+++ main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
 	"os/exec"
-	"path"
+	"regexp"
 	"time"
 
 	"github.com/bmatcuk/doublestar"
 )
 
 const (
-	IdeCmd  = "atom"
-	ExtList = "js,jsx,css,sass,scss"
+	IdeCmd = "atom"
 )
+
+func CheckFatalError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	cwd, err := os.Getwd()
@@ -27,24 +33,49 @@ func main() {
 	var (
 		interval float64
 		wd       string
+		glob     string
+		re2      string
 	)
 
 	flag.Float64Var(&interval, "interval", 5, "Refresh interval in minutes")
 	flag.StringVar(&wd, "path", cwd, "Working directory")
 
+	flag.StringVar(&glob, "glob", "**/*.{js,go}", "Glob for file paths")
+	flag.StringVar(&re2, "re2", "", "Re2 regexp for file contents. Example: \"(?i)caseinsensitivesubstring\" ")
+
 	flag.Parse()
 
-	pattern := path.Join(wd, "**/*.{"+ExtList+"}")
-	fmt.Println(pattern)
-	matches, err := doublestar.Glob(pattern)
-	if err != nil {
-		log.Fatal(err)
+	fmt.Println(glob)
+	matches, err := doublestar.Glob(glob)
+	CheckFatalError(err)
+
+	if len(re2) > 0 {
+		var (
+			m []string
+		)
+
+		r, err := regexp.Compile(re2)
+		CheckFatalError(err)
+
+		for _, filename := range matches {
+			content, err := ioutil.ReadFile(filename)
+			CheckFatalError(err)
+			if r.Match(content) {
+				m = append(m, filename)
+			}
+		}
+
+		matches = m
 	}
 
 	total := len(matches)
 
 	log.Println("Interval:", interval, "minutes")
 	log.Println("Working directory:", wd)
+	log.Println("Glob:", glob)
+	if len(re2) > 0 {
+		log.Println("Re2 regexp:", re2)
+	}
 	log.Println("Files founded:", total)
 
 	tickerInterval := time.Duration(interval * float64(time.Minute))
@@ -58,6 +89,7 @@ func main() {
 		if err != nil {
 			log.Panicln(err)
 		}
+
 		log.Println("->", name)
 	}
 }
